ce28b8ee8883031aa46b3f91838d5bbdcff29f78
not-zerith <116778283+DalpraX@users.noreply.github.com>
2026-09-23 21:57:13 -0500

    v1.0.0

---
+++ server_files/shaderpacks/_0EuphoriaPatches_ErrorShader/shaders/errorTexts.glsl
@@ -0,0 +1,35 @@
+beginTextError(3, offset);
+    printString((_equal, _equal, _equal, _space, _S, _H, _A, _D, _E, _R, _space, _N, _O, _T, _space, _F, _O, _U, _N, _D, _space, _equal, _equal, _equal));
+endText(textColor);
+
+offset.y += 1;
+
+beginTextError(3, offset);
+    printString((_R, _e, _q, _u, _i, _r, _e, _d, _colon, _space, _C, _o, _m, _p, _l, _e, _m, _e, _n, _t, _a, _r, _y, _S, _h, _a, _d, _e, _r, _s, _space, _r, _5, _dot, _7, _dot, _1));
+endText(textColor);
+
+offset.y += 1;
+
+beginTextError(3, offset);
+    printLine();
+endText(textColor);
+offset.y += 1;
+
+beginTextError(3, offset);
+    printString((_A, _space, _n, _e, _w, _e, _r, _space, _v, _e, _r, _s, _i, _o, _n, _space, _o, _f, _space, _E, _u, _p, _h, _o, _r, _i, _a, _P, _a, _t, _c, _h, _e, _s, _space, _i, _s, _space, _a, _v, _a, _i, _l, _a, _b, _l, _e, _exclm));
+endText(textColor);
+
+offset.y += 1;
+
+beginTextError(3, offset);
+    printString((_S, _O, _L, _U, _T, _I, _O, _N, _colon, _space, _U, _p, _d, _a, _t, _e, _space, _t, _o, _space, _v, _e, _r, _s, _i, _o, _n, _space, _1, _dot, _1, _0, _dot, _5, _space, _a, _l, _o, _n, _g, _space, _w, _i, _t, _h, _space, _t, _h, _e, _space, _c, _o, _m, _p, _a, _t, _i, _b, _l, _e, _space, _s, _h, _a, _d, _e, _r, _space, _v, _e, _r, _s, _i, _o, _n, _colon));
+    printLine();
+    printString((_C, _o, _m, _p, _l, _e, _m, _e, _n, _t, _a, _r, _y, _under, _r, _5, _dot, _9, _dot, _3));
+endText(textColor);
+
+offset.y += 2;
+
+beginTextError(3, offset);
+    printString((_D, _o, _w, _n, _l, _o, _a, _d, _space, _f, _r, _o, _m, _colon, _space, _h, _t, _t, _p, _s, _colon, _slash, _slash, _e, _u, _p, _h, _o, _r, _i, _a, _p, _a, _t, _c, _h, _e, _s, _dot, _c, _o, _m, _slash, _d, _o, _w, _n, _l, _o, _a, _d));
+endText(textColor);
+
